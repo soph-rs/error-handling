1e4fa6d4630317a79e7243ccaf7b45e6cdbc08b5
jiannei <longjian.huang@foxmail.com>
2024-08-29 13:30:29 +0800

    test: map_err

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let key = std::env::var("NUMBER_IN_ENV").unwrap();
+    let key = std::env::var("NUMBER_IN_ENV").map_err(|err| err)?;
     let number = key.parse::<i32>()?;
 
     println!("\"NUMBER_IN_ENV\" is {}", number);
