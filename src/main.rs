ed85031fc15e2045e7480990db43c8cfa48d371b
jiannei <longjian.huang@foxmail.com>
2024-08-29 16:27:07 +0800

    test: try std result

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use dotenvy::dotenv;
-use eyre::{eyre, Result, WrapErr};
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     color_eyre::install()?;
 
     let var = "NUMBER_IN_ENV";
 
-    let key = std::env::var(var).context(eyre!("Failed to get env var {var}"))?;
+    let key = std::env::var(var)?;
     let number = key.parse::<i32>()?;
 
     println!("\"NUMBER_IN_ENV\" is {}", number);
