f37b769636c31fb5131fa9469b162e0ba0a1a363
jiannei <longjian.huang@foxmail.com>
2024-08-29 13:49:04 +0800

    feat: anyhow context

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 fn main() -> Result<()> {
-    let key = std::env::var("NUMBER_IN_ENV")?;
+    let key = std::env::var("NUMBER_IN_ENV").context("Failed to get env var NUMBER_IN_ENV")?;
     let number = key.parse::<i32>()?;
 
     println!("\"NUMBER_IN_ENV\" is {}", number);
