5580e6d72349825231d6dfaf14b95c0b0f76b2ef
jiannei <longjian.huang@foxmail.com>
2024-08-29 13:12:20 +0800

    feat: unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let key = std::env::var("NUMBER_IN_ENV").unwrap();
+    let number = key.parse::<i32>().unwrap();
+
+    println!("\"NUMBER_IN_ENV\" is {}", number);
 }
