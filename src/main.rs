4c77fd2912d4a06505c1ad186e3ec2697d61895d
jiannei <longjian.huang@foxmail.com>
2024-08-29 16:04:16 +0800

    feat: eyre!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use eyre::{Result, WrapErr};
+use eyre::{eyre, Result, WrapErr};
 fn main() -> Result<()> {
     let var = "NUMBER_IN_ENV";
 
-    let key = std::env::var(var)
-        .wrap_err_with(|| format!("Failed to get env var {var}"))
-        .expect("more error info");
+    let key = std::env::var(var).wrap_err(eyre!("Failed to get env var {var}"))?;
     let number = key.parse::<i32>()?;
 
     println!("\"NUMBER_IN_ENV\" is {}", number);
