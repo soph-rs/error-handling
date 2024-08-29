7491e32625e76d6edc01b1d863525c0bf9850b56
jiannei <longjian.huang@foxmail.com>
2024-08-29 15:02:45 +0800

    feat: eyre

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let key = std::env::var("NUMBER_IN_ENV").map_err(|err| format!("{err} (NUMBER_IN_ENV)"))?;
+use eyre::Result;
+fn main() -> Result<()> {
+    let key = std::env::var("NUMBER_IN_ENV")?;
     let number = key.parse::<i32>()?;
 
     println!("\"NUMBER_IN_ENV\" is {}", number);
