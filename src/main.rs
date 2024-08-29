3e6db2f2b7c48f2d6ef9379988410fb1967d9e70
jiannei <longjian.huang@foxmail.com>
2024-08-29 13:17:10 +0800

    feat: expect

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let key = std::env::var("NUMBER_IN_ENV").unwrap();
+    let key = std::env::var("NUMBER_IN_ENV").expect("Failed to get env var NUMBER_IN_ENV");
     let number = key.parse::<i32>().unwrap();
 
     println!("\"NUMBER_IN_ENV\" is {}", number);
