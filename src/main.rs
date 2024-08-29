22df2f3e88a112a814d660cd81172003c2ceb2c1
jiannei <longjian.huang@foxmail.com>
2024-08-29 13:25:39 +0800

    test: try unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let key = std::env::var("NUMBER_IN_ENV")?;
+    let key = std::env::var("NUMBER_IN_ENV").unwrap();
     let number = key.parse::<i32>()?;
 
     println!("\"NUMBER_IN_ENV\" is {}", number);
